1efd7e0f84a23d17ed404290e44f4a13ecfb4d49
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-18 19:21:42 -0700

    LC-maximum-difference-between-increasing-elements [Runtime: 4 ms; Memory: 18.1 MB]

--- array/maximum-difference-between-increasing-elements.py
+++ array/maximum-difference-between-increasing-elements.py
@@ -1,9 +1,9 @@
 class Solution:
     def maximumDifference(self, nums: List[int]) -> int:
-        max_diff = -1
+        max_diff = 0
         curr = nums[0]
         for num in nums[1:]:
             max_diff = max(max_diff, num - curr)
             curr = min(curr, num)
 
-        return max_diff
+        return max_diff if max_diff > 0 else -1
