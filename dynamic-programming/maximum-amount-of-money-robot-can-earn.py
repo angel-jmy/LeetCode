292a550eee9a550453164cdfbdc77588c1764f82
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-18 17:06:44 -0700

    LC-maximum-amount-of-money-robot-can-earn [Runtime: 6330 ms; Memory: 144.6 MB]

--- dynamic-programming/maximum-amount-of-money-robot-can-earn.py
+++ dynamic-programming/maximum-amount-of-money-robot-can-earn.py
@@ -11,28 +11,18 @@ class Solution:
             for j in range(n):
                 if i == 0 and j == 0:
                     continue
-                if j == 0:
+                if i > 0:
                     for k in dp[i - 1][j]:
                         dp[i][j][k] = max(dp[i][j][k], dp[i - 1][j][k] + coins[i][j])
                         if coins[i][j] < 0 and k < 2:
                             dp[i][j][k + 1] = max(dp[i][j][k + 1], dp[i - 1][j][k])
 
-                elif i == 0:
+                if j > 0:
                     for k in dp[i][j - 1]:          
                         dp[i][j][k] = max(dp[i][j][k], dp[i][j - 1][k] + coins[i][j])
                         if coins[i][j] < 0 and k < 2:
                             dp[i][j][k + 1] = max(dp[i][j][k + 1], dp[i][j - 1][k])
                 
-                else:
-                    for k in dp[i - 1][j]:
-                        dp[i][j][k] = max(dp[i][j][k], dp[i - 1][j][k] + coins[i][j])
-                        if coins[i][j] < 0 and k < 2:
-                            dp[i][j][k + 1] = max(dp[i][j][k + 1], dp[i - 1][j][k])
-
-                    for k in dp[i][j - 1]:
-                        dp[i][j][k] = max(dp[i][j][k], dp[i][j - 1][k] + coins[i][j])
-                        if coins[i][j] < 0 and k < 2:
-                            dp[i][j][k + 1] = max(dp[i][j][k + 1], dp[i][j - 1][k])
         
         return max(dp[m - 1][n - 1].values())
                 
