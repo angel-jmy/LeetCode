63c32efbac8151f226f33ec05f91fc5f9c720bc2
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-18 18:51:22 -0700

    LC-number-of-pairs-of-interchangeable-rectangles [Runtime: 65 ms; Memory: 63.3 MB]

--- array/number-of-pairs-of-interchangeable-rectangles.py
+++ array/number-of-pairs-of-interchangeable-rectangles.py
@@ -1,12 +1,23 @@
 class Solution:
     def interchangeableRectangles(self, rectangles: List[List[int]]) -> int:
-        counts = defaultdict(int)
+        counts = {}
+        pairs = 0
         for w, h in rectangles:
             ratio = w / h
-            counts[ratio] += 1
+            if ratio in counts:
+                pairs += counts[ratio]
 
-        pairs = 0
-        for n in counts.values():
-            pairs += n * (n - 1) // 2
+            counts[ratio] = counts.get(ratio, 0) + 1
 
         return pairs
+
+        # counts = defaultdict(int)
+        # for w, h in rectangles:
+        #     ratio = w / h
+        #     counts[ratio] += 1
+
+        # pairs = 0
+        # for n in counts.values():
+        #     pairs += n * (n - 1) // 2
+
+        # return pairs
