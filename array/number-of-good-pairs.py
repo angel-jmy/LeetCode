49f62ffbcf8a980fe27912b33a2bce4d1c2cd89e
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-18 18:34:35 -0700

    LC-number-of-good-pairs [Runtime: 0 ms; Memory: 17.9 MB]

---
+++ array/number-of-good-pairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        seen = defaultdict(int)
+        pairs = 0
+        for i in range(len(nums)):
+            seen[nums[i]] += 1
+        
+        for n in seen.values():
+            pairs += n * (n - 1) / 2
+
+        return int(pairs)
