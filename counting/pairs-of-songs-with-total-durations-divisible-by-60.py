a76d2777e8e58db317ba2a8e860059b0a2de5226
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-20 16:34:44 -0700

    LC-pairs-of-songs-with-total-durations-divisible-by-60 [Runtime: 32 ms; Memory: 21.3 MB]

--- counting/pairs-of-songs-with-total-durations-divisible-by-60.py
+++ counting/pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -2,16 +2,19 @@ class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
         mods = {}
         pairs = 0
+        self_pairs = 0
         for t in time:
             curr_mod = t % 60
             if curr_mod == 0:
-                pairs += 1
+                self_pairs += 1
                 continue
             comp = 60 - curr_mod
             if comp in mods.keys():
                 pairs += mods[comp]
 
             mods[curr_mod] = mods.get(curr_mod, 0) + 1
+
+        pairs += self_pairs * (self_pairs - 1) // 2
 
         return pairs
             
