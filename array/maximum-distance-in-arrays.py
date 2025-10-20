8e272ea9fe3601053ea0d042f6d32409dd9e4449
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-19 20:35:56 -0700

    LC-maximum-distance-in-arrays [Runtime: 65 ms; Memory: 33.7 MB]

--- array/maximum-distance-in-arrays.py
+++ array/maximum-distance-in-arrays.py
@@ -1,47 +1,60 @@
 class Solution:
     def maxDistance(self, arrays: List[List[int]]) -> int:
-        min1, min2 = float('inf'), float('inf')
-        max1, max2 = -float('inf'), -float('inf')
+        smallest = arrays[0][0]
+        biggest = arrays[0][-1]
+        max_distance = 0
 
-        min_idx1, min_idx2 = -1, -1
-        max_idx1, max_idx2 = -1, -1
+        for i in range(1, len(arrays)):
+            arr = arrays[i]
+            max_distance = max(max_distance, abs(arr[-1] - smallest), abs(biggest - arr[0]))
+            smallest = min(smallest, arr[0])
+            biggest = max(biggest, arr[-1])
 
-        for i in range(len(arrays)):
-            l = arrays[i]
+        return max_distance
+        
+        
+        # min1, min2 = float('inf'), float('inf')
+        # max1, max2 = -float('inf'), -float('inf')
 
-            curr_min, curr_max = l[0], l[-1]
-            if min1 <= curr_min < min2:
-                min2 = curr_min
-                min_idx2 = i
+        # min_idx1, min_idx2 = -1, -1
+        # max_idx1, max_idx2 = -1, -1
 
-            elif curr_min < min1:
-                min2 = min1
-                min_idx2 = min_idx1
-                min1 = curr_min
-                min_idx1 = i
+        # for i in range(len(arrays)):
+        #     l = arrays[i]
 
-            if max1 >= curr_max > max2:
-                max2 = curr_max
-                max_idx2 = i
+        #     curr_min, curr_max = l[0], l[-1]
+        #     if min1 <= curr_min < min2:
+        #         min2 = curr_min
+        #         min_idx2 = i
 
-            elif curr_max > max1:
-                max2 = max1
-                max_idx2 = max_idx1
-                max1 = curr_max
-                max_idx1 = i
+        #     elif curr_min < min1:
+        #         min2 = min1
+        #         min_idx2 = min_idx1
+        #         min1 = curr_min
+        #         min_idx1 = i
 
-        max_diff = 0
-        for i in range(len(arrays)):
-            l = arrays[i]
-            curr_min, curr_max = l[0], l[-1]
-            if i == max_idx1:
-                max_diff = max(max_diff, max2 - curr_min)
-            else:
-                max_diff = max(max_diff, max1 - curr_min)
+        #     if max1 >= curr_max > max2:
+        #         max2 = curr_max
+        #         max_idx2 = i
 
-            if i == min_idx1:
-                max_diff = max(max_diff, curr_max - min2)
-            else:
-                max_diff = max(max_diff, curr_max - min1)
+        #     elif curr_max > max1:
+        #         max2 = max1
+        #         max_idx2 = max_idx1
+        #         max1 = curr_max
+        #         max_idx1 = i
 
-        return max_diff
+        # max_diff = 0
+        # for i in range(len(arrays)):
+        #     l = arrays[i]
+        #     curr_min, curr_max = l[0], l[-1]
+        #     if i == max_idx1:
+        #         max_diff = max(max_diff, max2 - curr_min)
+        #     else:
+        #         max_diff = max(max_diff, max1 - curr_min)
+
+        #     if i == min_idx1:
+        #         max_diff = max(max_diff, curr_max - min2)
+        #     else:
+        #         max_diff = max(max_diff, curr_max - min1)
+
+        # return max_diff
