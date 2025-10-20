7a2815dc10b118b693fe851795885de844ffed63
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-19 19:49:23 -0700

    LC-maximum-distance-in-arrays [Runtime: N/A; Memory: N/A]

--- greedy/maximum-distance-in-arrays.py
+++ greedy/maximum-distance-in-arrays.py
@@ -2,6 +2,7 @@ class Solution:
     def maxDistance(self, arrays: List[List[int]]) -> int:
         max_diff = 0
         ranges = [[x[0], x[-1]] for x in arrays]
-        ranges.sort(key = lambda x: (x[0], x[1]))
+        ranges1 = sorted(ranges, key = lambda x: (x[0], x[1]))
+        ranges2 = sorted(ranges, key = lambda x: (-x[1], -x[0]))
 
-        return ranges[-1][-1] - ranges[0][0]
+        return max(ranges1[-1][-1] - ranges1[0][0], ranges2[0][1] - ranges2[-1][0])
