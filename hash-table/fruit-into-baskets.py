90a7afb796c9a4a67a33a62b42630790752f1224
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-21 16:59:36 -0700

    LC-fruit-into-baskets [Runtime: N/A; Memory: N/A]

--- hash-table/fruit-into-baskets.py
+++ hash-table/fruit-into-baskets.py
@@ -5,17 +5,20 @@ class Solution:
         max_len = 0
 
         counts = defaultdict(int)
-        
+        types = 0
+
         for r in range(N):
-            if fruits[r] not in counts and len(counts) <= 1:
+            if fruits[r] not in counts and types <= 1:
                 counts[fruits[r]] += 1
+                types += 1
             elif fruits[r] in counts:
                 counts[fruits[r]] += 1
             else:
-                while fruits[r] not in counts and len(counts) >= 2:
+                while fruits[r] not in counts and types >= 2:
                     counts[fruits[l]] -= 1
                     if counts[fruits[l]] == 0:
                         del counts[fruits[l]]
+                        types -= 1
 
                     l += 1
 
