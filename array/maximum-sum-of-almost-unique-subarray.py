ba379b8f1f5c7041f12df7c6d69c9c6b443044c4
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-20 20:07:27 -0700

    LC-maximum-sum-of-almost-unique-subarray [Runtime: N/A; Memory: N/A]

---
+++ array/maximum-sum-of-almost-unique-subarray.py
@@ -0,0 +1,43 @@
+class Solution:
+    def maxSum(self, nums: List[int], m: int, k: int) -> int:
+        N = len(nums)
+        n_not_unique = 0
+        curr_sum = max_sum = 0
+        counts = defaultdict(int)
+        for num in nums[:k]:
+            if num not in counts:
+                counts[num] += 1
+            else:
+                n_not_unique += 1
+                counts[num] += 1
+            curr_sum += num
+
+        if n_not_unique <= k - m:
+            max_sum = max(max_sum, curr_sum)
+
+        for i in range(k, N):
+            left = i - k
+            if nums[left] == nums[i]:
+                continue
+
+            if nums[left] > 2:
+                counts[nums[left]] -= 1
+            elif nums[left] == 2:
+                counts[nums[left]] -= 1
+                n_not_unique -= 1
+            else:
+                del counts[nums[left]]
+            
+            if nums[i] in counts:
+                n_not_unique += 1
+            
+            counts[nums[i]] += 1
+
+            curr_sum = curr_sum + nums[i] - nums[left]
+
+            if n_not_unique <= k - m:
+                max_sum = max(max_sum, curr_sum)
+
+        return max_sum
+        
+
