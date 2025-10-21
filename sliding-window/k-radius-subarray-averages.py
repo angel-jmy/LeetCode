1e9c90463e0c58b5011edb252a0eeb82777b9236
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-20 19:22:37 -0700

    LC-k-radius-subarray-averages [Runtime: 63 ms; Memory: 32.1 MB]

--- sliding-window/k-radius-subarray-averages.py
+++ sliding-window/k-radius-subarray-averages.py
@@ -6,11 +6,13 @@ class Solution:
         if first_i + k > N - 1:
             return avg
 
-        avg[first_i] = sum(nums[first_i - k:first_i + k + 1]) / (2 * k + 1)
+        window_sum = sum(nums[first_i - k:first_i + k + 1])
+        avg[first_i] = window_sum // (2 * k + 1)
 
         for i in range(first_i + 1, N - k):
             left = i - k - 1
             right = i + k
-            avg[i] = avg[i - 1] + (nums[right] - nums[left]) / (2 * k + 1)
+            window_sum = window_sum + nums[right] - nums[left]
+            avg[i] = window_sum // (2 * k + 1)
 
-        return [int(num) for num in avg]
+        return avg
