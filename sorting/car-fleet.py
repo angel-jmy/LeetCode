6e33e8448954f18df4d1738fddaad380935fdb53
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-23 01:17:51 -0700

    LC-car-fleet [Runtime: N/A; Memory: N/A]

---
+++ sorting/car-fleet.py
@@ -0,0 +1,15 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        N = len(position)
+        times = list(map(lambda pos, sp: (target - pos) / sp, position, speed))
+        combined = list(zip(position, times))
+        combined.sort(key = lambda x: x[0])
+
+        stack = []
+        for i in range(N - 1, -1, -1):
+            while stack and combined[i][1] <= combined[stack[-1]][1]:
+                stack.pop()               
+
+            stack.append(i)
+
+        return len(stack)
