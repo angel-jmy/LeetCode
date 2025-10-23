38edb1d584279fbe3e01f62fb14a0383ecb7adcf
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-22 17:52:58 -0700

    LC-validate-stack-sequences [Runtime: N/A; Memory: N/A]

---
+++ array/validate-stack-sequences.py
@@ -0,0 +1,28 @@
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack = []
+        i = j = 0
+        N = len(pushed)
+
+        while i < N and j < N:
+            if pushed[i] != popped[j]:
+                if stack and stack[-1] == popped[j]:
+                    stack.pop()
+                    j += 1
+                else:
+                    stack.append(pushed[i])
+                    i += 1
+            elif pushed[i] == popped[j]:
+                stack.append(pushed[i])
+                stack.pop()
+                i += 1
+                j += 1
+
+        if j < N:
+            if stack and popped[j] != stack[-1]:
+                return False
+            if not stack:
+                return False
+
+
+        return True
