b48f490d29959737f53548a1237b1bfe51749e4a
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-23 19:04:16 -0700

    LC-minimum-operations-to-reduce-x-to-zero [Runtime: N/A; Memory: N/A]

---
+++ sliding-window/minimum-operations-to-reduce-x-to-zero.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        N = len(nums)
+        l, r = 0, 0
+        diff = sum(nums) - x
+        curr_sum = 0
+
+        max_len = 0
+
+        while r < N:
+            curr_sum += nums[r]
+            while curr_sum > diff:
+                curr_sum -= nums[l]
+                l += 1
+
+            if curr_sum == diff:
+                max_len = max(max_len, r - l + 1)
+                curr_sum -= nums[l]
+                l += 1
+
+            r += 1
+        return N - max_len if max_len != 0 else -1
+
+
+        
