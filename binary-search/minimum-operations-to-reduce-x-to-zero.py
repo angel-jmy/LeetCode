393939d3c490fdd548e4725f4930d8a322e68b02
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-23 19:15:50 -0700

    LC-minimum-operations-to-reduce-x-to-zero [Runtime: 121 ms; Memory: 40.6 MB]

--- binary-search/minimum-operations-to-reduce-x-to-zero.py
+++ binary-search/minimum-operations-to-reduce-x-to-zero.py
@@ -1,28 +1,46 @@
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
         N = len(nums)
-        l, r = 0, 0
-        diff = sum(nums) - x
-        if diff == 0:
+        target = sum(nums) - x
+        if target == 0:
             return N
+        prefix = {0: -1}
+        curr = 0
+        max_len = -1
 
-        curr_sum = 0
+        for i, num in enumerate(nums):
+            curr += num
+            if curr - target in prefix:
+                max_len = max(max_len, i - prefix[curr - target])
+            prefix[curr] = i
 
-        max_len = 0
+        return N - max_len if max_len != -1 else -1
+        
+        
+        
+        # N = len(nums)
+        # l, r = 0, 0
+        # diff = sum(nums) - x
+        # if diff == 0:
+        #     return N
 
-        for r in range(N):
-            curr_sum += nums[r]
-            while curr_sum > diff and l < N:
-                curr_sum -= nums[l]
-                l += 1
+        # curr_sum = 0
 
-            if curr_sum == diff and l < N:
-                max_len = max(max_len, r - l + 1)
-                curr_sum -= nums[l]
-                l += 1
+        # max_len = 0
+
+        # for r in range(N):
+        #     curr_sum += nums[r]
+        #     while curr_sum > diff and l < N:
+        #         curr_sum -= nums[l]
+        #         l += 1
+
+        #     if curr_sum == diff and l < N:
+        #         max_len = max(max_len, r - l + 1)
+        #         curr_sum -= nums[l]
+        #         l += 1
 
 
-        return N - max_len if max_len != 0 else -1
+        # return N - max_len if max_len != 0 else -1
 
 
         
