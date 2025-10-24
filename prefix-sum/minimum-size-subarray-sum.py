108734d1a92bb9595f510625e477a54f5b5aac3c
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-23 20:03:14 -0700

    LC-minimum-size-subarray-sum [Runtime: 7 ms; Memory: 28.2 MB]

--- prefix-sum/minimum-size-subarray-sum.py
+++ prefix-sum/minimum-size-subarray-sum.py
@@ -1,24 +1,15 @@
-class Solution(object):
-    def minSubArrayLen(self, target, nums):
-        """
-        :type target: int
-        :type nums: List[int]
-        :rtype: int
-        """
-        # nums = sorted(nums, reverse = True)
-        curr_sum = 0
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         N = len(nums)
-        min_len = N + 1
         l, r = 0, 0
-        # length = 0
+        curr_sum = 0
+        min_len = N + 1
 
         for r in range(N):
             curr_sum += nums[r]
-            # length += 1
             while curr_sum >= target:
                 min_len = min(min_len, r - l + 1)
                 curr_sum -= nums[l]
-                l += 1               
-            # r += 1
+                l += 1
 
-        return min_len if min_len <= N else 0
+        return min_len if min_len != N + 1 else 0
