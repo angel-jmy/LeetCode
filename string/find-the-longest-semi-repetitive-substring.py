60513ca9c50ccf16ad36fcf7db3f7607790d92ba
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-23 19:55:36 -0700

    LC-find-the-longest-semi-repetitive-substring [Runtime: 16 ms; Memory: 17.9 MB]

--- string/find-the-longest-semi-repetitive-substring.py
+++ string/find-the-longest-semi-repetitive-substring.py
@@ -3,27 +3,22 @@ class Solution:
         counts = defaultdict(int)
         N = len(s)
         l, r = 0, 0
-        pairs = 0
         max_len = 0
+        pairs = 0
 
         for r in range(N):
-            counts[s[r]] += 1
-            if counts[s[r]] >= 2:
+            if r > 0 and s[r] == s[r - 1]:
                 pairs += 1
 
-            while pairs >= 2:
-                left = s[l]
-                if counts[left] == 2:
-                    counts[left] -= 1
+            while pairs > 1:
+                if l + 1 <= r and s[l] == s[l + 1]:
                     pairs -= 1
-                    l += 1    
-                else:
-                    counts[left] -= 1
                     l += 1
+                else:
+                    l += 1
+
             max_len = max(max_len, r - l + 1)
 
         return max_len
-
-            
 
 
