0c7192dc6b57d8a23e1cb22846d86bf90a228400
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-25 15:01:08 -0700

    LC-grumpy-bookstore-owner [Runtime: 24 ms; Memory: 19.7 MB]

--- sliding-window/grumpy-bookstore-owner.py
+++ sliding-window/grumpy-bookstore-owner.py
@@ -1,26 +1,26 @@
 class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
         N = len(customers)
-        total = sum(customers)
         window = [-1, -1]
-        max_num = 0
+        max_add = 0
         curr_num = 0
         l = 0
 
         for r in range(N):
-            curr_num += customers[r]
+            curr_num += customers[r] * grumpy[r]
             if r - minutes + 1 > 0:
                 l = r - minutes
-                curr_num -= customers[l]
+                curr_num -= customers[l] * grumpy[l]
                 l += 1
 
-            if curr_num > max_num:
-                max_num = curr_num
+            if curr_num > max_add:
+                max_add = curr_num
                 window = [l, r]
 
-        rest = sum([customers[i] * (1 - grumpy[i]) for i in range(N) if i < window[0] or i > window[1]])
+        total = sum([customers[i] * (1 - grumpy[i]) for i in range(N)])
         print(window)
-        print(rest)
+        print(max_add)
 
-        return rest + max_num
+
+        return total + max_add
 
