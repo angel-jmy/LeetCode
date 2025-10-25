f067e9bb74412bab41c2583b4e49cd269851e07a
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-25 00:39:50 -0700

    LC-minimum-discards-to-balance-inventory [Runtime: 251 ms; Memory: 108.3 MB]

--- counting/minimum-discards-to-balance-inventory.py
+++ counting/minimum-discards-to-balance-inventory.py
@@ -1,18 +1,19 @@
 class Solution:
     def minArrivalsToDiscard(self, arrivals: List[int], w: int, m: int) -> int:
         N = len(arrivals)
-        counts = defaultdict(int)
+        # counts = defaultdict(int)
         min_disc = 0
+        dq = defaultdict(lambda: deque())
 
         for i in range(N):
-            if i < w:
-                counts[arrivals[i]] += 1
-            else:
-                counts[arrivals[i]] += 1
-                counts[arrivals[i - w]] -= 1
+            x = arrivals[i]
+            while dq[x] and dq[x][0] < i - w + 1:
+                dq[x].popleft()
 
-            while counts[arrivals[i]] > m:
-                counts[arrivals[i]] -= 1
+            if len(dq[x]) < m:
+                dq[x].append(i)
+            elif len(dq[x]) == m:
                 min_disc += 1
+
 
         return min_disc
