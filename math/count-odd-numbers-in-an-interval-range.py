c41d3c4c2d8aacb3b0db1f69be0d1814a071c205
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-27 15:47:42 -0700

    LC-count-odd-numbers-in-an-interval-range [Runtime: 26 ms; Memory: 17.7 MB]

--- math/count-odd-numbers-in-an-interval-range.py
+++ math/count-odd-numbers-in-an-interval-range.py
@@ -1,10 +1,9 @@
 class Solution:
     def countOdds(self, low: int, high: int) -> int:
-        if (high - low) % 2:
+        if (high - low) % 2 or (low % 2 and high % 2):
             return (high - low) // 2 + 1
-        if low % 2 and high % 2:
-            return (high - low) // 2 + 1
-        
+      
+      
         return (high - low) // 2
 
         
