8d18dcfa7892565e352c62b9d162e3201bd2a22e
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-27 15:43:09 -0700

    LC-maximum-subarray [Runtime: 87 ms; Memory: 32.6 MB]

--- array/maximum-subarray.py
+++ array/maximum-subarray.py
@@ -1,28 +1,10 @@
-class Solution(object):
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        # cur_max, max_till_now = 0, -float('inf')
-        # for c in nums:
-        #     cur_max = max(c, cur_max + c)
-        #     max_till_now = max(max_till_now, cur_max)
-        # return max_till_now
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxsum = nums[0]
+        cursum = maxsum
+        for num in nums[1:]:
+            cursum = max(cursum + num, num)
+            maxsum = max(maxsum, cursum)
 
-        cur = best = nums[0]
-        start = 0
-        best_l = best_r = 0
-
-        for i in range(1, len(nums)):
-            if nums[i] > cur + nums[i]:
-                cur = nums[i]
-                start = i         # start new subarray at i
-            else:
-                cur += nums[i]    # extend
-
-            if cur > best:
-                best = cur
-                best_l, best_r = start, i
-
-        return best
+        return maxsum
+        
