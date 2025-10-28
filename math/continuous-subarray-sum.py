ceb80b9ac1ef167ca9ff88af9f2e549b99a91d0a
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-27 21:12:25 -0700

    LC-continuous-subarray-sum [Runtime: 48 ms; Memory: 38.1 MB]

--- math/continuous-subarray-sum.py
+++ math/continuous-subarray-sum.py
@@ -1,7 +1,7 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         N = len(nums)
-        cache = {0: -1}
+        cache = {0: -1} # Recording the index of the first seen MOD
         cur_sum = 0
 
         for i in range(N):
