e580a9d3b2fe4fc4c62ed339ea89ee80c83940fd
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-27 21:11:59 -0700

    LC-continuous-subarray-sum [Runtime: 51 ms; Memory: 38.3 MB]

---
+++ math/continuous-subarray-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        N = len(nums)
+        cache = {0: -1}
+        cur_sum = 0
+
+        for i in range(N):
+            cur_sum += nums[i]
+            mod = cur_sum % k
+            if mod in cache:
+                if i - cache[mod] >= 2:
+                    return True
+
+            else:
+                cache[mod] = i
+
+        return False
