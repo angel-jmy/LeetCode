2aa85a503e4dafd9ddbc927adbe6a23778b2dddf
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-27 23:34:53 -0700

    LC-replace-the-substring-for-balanced-string [Runtime: 227 ms; Memory: 18.3 MB]

--- string/replace-the-substring-for-balanced-string.py
+++ string/replace-the-substring-for-balanced-string.py
@@ -3,14 +3,17 @@ class Solution:
         N = len(s)
         l, r = 0, 0
         counts = Counter(s)
+        if all([v <= N // 4 for v in counts.values()]):
+            return 0
+
         min_len = N + 1
 
         for r in range(N):
             counts[s[r]] -= 1
-            while all([v <= N // 4 for v in counts.values()]) and l < N:
+            while all([v <= N // 4 for v in counts.values()]) and l <= r:
                 min_len = min(min_len, r - l + 1)
                 counts[s[l]] += 1
                 l += 1
             
-        return min_len if min_len != N + 1 else 0
+        return min_len 
 
