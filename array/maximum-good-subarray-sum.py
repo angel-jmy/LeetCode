3de11ffa151512e1d25e0d052a5c6204ed747fad
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-28 00:13:32 -0700

    LC-maximum-good-subarray-sum [Runtime: 226 ms; Memory: 29.5 MB]

--- array/maximum-good-subarray-sum.py
+++ array/maximum-good-subarray-sum.py
@@ -1,27 +1,30 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
         N = len(nums)
-        pref = [0] * (N + 1)
+        pref = 0 # Current pref
         max_sum = -float('inf')
 
-        hashmap = defaultdict(list)
+        hashmap = {} # Best/minimum pref at a given number
 
         for i in range(N):
             num = nums[i]
-            pref[i + 1] = pref[i] + num
-
+            
             comp1 = num + k
             comp2 = num - k
+
             if comp1 in hashmap:
-                for left in hashmap[comp1]:
-                    cur_sum = pref[i + 1] - pref[left]
-                    max_sum = max(max_sum, cur_sum)
+                cur_sum = pref + num - hashmap[comp1]
+                max_sum = max(max_sum, cur_sum)
 
             if comp2 in hashmap:
-                for left in hashmap[comp2]:
-                    cur_sum = pref[i + 1] - pref[left]
-                    max_sum = max(max_sum, cur_sum)
+                cur_sum = pref + num - hashmap[comp2]
+                max_sum = max(max_sum, cur_sum)
 
-            hashmap[num].append(i)
+            if num in hashmap:
+                hashmap[num] = min(hashmap[num], pref)
+            else:
+                hashmap[num] = pref
+
+            pref += num
 
         return max_sum if max_sum != -float('inf') else 0
