ee04102c1e19e5c5965f6259473e6b2271b79b82
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-28 17:16:47 -0700

    LC-number-of-beautiful-pairs [Runtime: 163 ms; Memory: 17.9 MB]

---
+++ array/number-of-beautiful-pairs.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countBeautifulPairs(self, nums: List[int]) -> int:
+        N = len(nums)
+        coprime = {l:[r for r in range(10) if gcd(l, r) == 1] for l in range(10)}
+        cache = {} # Record the first digit of every number
+
+        l, r = 0, 0
+        counter = 0
+
+        first_last = lambda num: (int(str(num)[0]), int(str(num)[-1]))
+
+        for r in range(N):
+            first, last = first_last(nums[r])
+            coprimes = coprime[last]
+            print(first, last)
+            print(coprimes)
+            new_count = 0
+            for d in cache:
+                if d in coprimes:
+                    new_count += cache.get(d, 0)
+            counter += new_count
+            print("counter:", counter)
+
+            cache[first] = cache.get(first, 0) + 1
+            print("cache:", cache)
+
+        return counter
