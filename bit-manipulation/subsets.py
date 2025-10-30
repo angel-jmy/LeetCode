6b67fe99982319a05b09c8813098411ffa785c96
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-29 20:10:00 -0700

    LC-subsets [Runtime: 0 ms; Memory: 18.1 MB]

--- bit-manipulation/subsets.py
+++ bit-manipulation/subsets.py
@@ -1,42 +1,18 @@
-class Solution(object):
-    def subsets(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        # res = []
-        # N = len(nums)
-
-        # def backtrack(idx, path, k0, k):
-        #     if k0 == k:
-        #         res.append(path[:])
-        #         return
-
-        #     for i in range(idx, N):
-        #         path.append(nums[i])
-        #         backtrack(i + 1, path, k0 + 1, k)
-        #         path.pop()
-
-
-        # for k in range(N + 1):
-        #     backtrack(0, [], 0, k)
-                  
-
-        # return res
-
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-        
-        def create_subset(path, i):
-            if i == len(nums):
+
+        def backtrack(idx, path):
+            if idx == len(nums):
                 res.append(path[:])
                 return
-            
-            path.append(nums[i])
-            create_subset(path, i+1)
 
+            path.append(nums[idx])
+            backtrack(idx + 1, path)
             path.pop()
-            create_subset(path, i+1)
+            backtrack(idx + 1, path)
 
-        create_subset([], 0)
+
+        backtrack(0, [])
 
         return res
