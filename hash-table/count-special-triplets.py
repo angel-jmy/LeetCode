fbb98f2730c1f5afbb63f15f248b662350703bb0
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-30 01:08:25 -0700

    LC-count-special-triplets [Runtime: 887 ms; Memory: 42.4 MB]

--- hash-table/count-special-triplets.py
+++ hash-table/count-special-triplets.py
@@ -1,24 +1,19 @@
 class Solution:
     def specialTriplets(self, nums: List[int]) -> int:
-        MOD = 10 ** 9 + 7
+        MOD = 10**9 + 7
         N = len(nums)
-        prefs = [defaultdict(int)] * N
-        suffs = [defaultdict(int)] * N
+        if N < 3:
+            return 0
 
-        prefs[0][nums[0]] = 1
-        suffs[0][nums[N - 1]] = 1
-
-        for i in range(1, N):
-            prefs[i] = prefs[i - 1].copy()
-            prefs[i][nums[i]] += 1
-
-        for i in range(N - 2, -1, -1):
-            suffs[i] = suffs[i + 1].copy()
-            suffs[i][nums[i]] += 1
+        left = Counter(nums[:1])          # counts of [0]
+        right = Counter(nums[2:])         # counts of [2..N-1]
 
         res = 0
         for j in range(1, N - 1):
             num = nums[j]
-            res = (res + prefs[j - 1][num * 2] * suffs[j + 1][num * 2]) % MOD
+            res = (res + left[2*num] * right[2*num]) % MOD
+            left[num] += 1
+            if j + 1 < N:
+                right[nums[j + 1]] -= 1
 
         return res
