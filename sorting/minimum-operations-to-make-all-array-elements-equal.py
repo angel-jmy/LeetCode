b02f0dd47b1168efb8a79170396311aaf8ed313e
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-31 19:01:27 -0700

    LC-minimum-operations-to-make-all-array-elements-equal [Runtime: N/A; Memory: N/A]

--- sorting/minimum-operations-to-make-all-array-elements-equal.py
+++ sorting/minimum-operations-to-make-all-array-elements-equal.py
@@ -7,32 +7,25 @@ class Solution:
         for i in range(1, N):
             prefs[i] = prefs[i - 1] + nums[i]
 
-        suffs = [0] * N
-        suffs[N - 1] = nums[N - 1]
-        for i in range(N - 2, -1, -1):
-            suffs[i] = suffs[i + 1] + nums[i]
+        total = prefs[-1]
 
-        def binary_search(target):
-            l, r = 0, N - 1
-            while l <= r:
-                mid = l + (r - l) // 2
-                if nums[mid] > target:
-                    r = mid - 1
-                else:
-                    l = mid + 1
-            return r
 
         res = []
         for q in queries:
             # print(q)
-            idx = binary_search(q)
-            # print(idx)
-            if q < nums[0]:
-                ops = suffs[0] - q * N
-            elif idx + 1 < N:
-                ops = q * (idx + 1) - prefs[idx] + suffs[idx + 1] - q * (N - idx - 1)
-            else:
-                ops = q * (idx + 1) - prefs[idx]
+            idx = bisect_right(nums, q)
+            print(idx)
+            left = q * idx - prefs[idx]
+            right = (total - prefs[idx]) * q * (N - idx)
+
+            ops = left + right
+
+            # if q < nums[0]:
+            #     ops = suffs[0] - q * N
+            # elif idx + 1 < N:
+            #     ops = q * (idx + 1) - prefs[idx] + suffs[idx + 1] - q * (N - idx - 1)
+            # else:
+            #     ops = q * (idx + 1) - prefs[idx]
             res.append(ops)
 
         return res
