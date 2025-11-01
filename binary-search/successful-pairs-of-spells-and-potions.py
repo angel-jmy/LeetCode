9e9946df0b21dc428445c833b95a1852594cc9a6
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-31 23:37:54 -0700

    LC-successful-pairs-of-spells-and-potions [Runtime: 211 ms; Memory: 42.7 MB]

---
+++ binary-search/successful-pairs-of-spells-and-potions.py
@@ -0,0 +1,14 @@
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        N, M = len(spells), len(potions)
+        # spells.sort()
+        potions.sort()
+
+        res = []
+        for s in spells:
+            comp = success / s
+            i = bisect_left(potions, comp)
+            res.append(M - i)
+
+        return res
+
