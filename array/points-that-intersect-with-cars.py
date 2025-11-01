c780a3fce1a92d77eafe04634a473ca397417dd9
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-11-01 16:20:07 -0700

    LC-points-that-intersect-with-cars [Runtime: N/A; Memory: N/A]

--- array/points-that-intersect-with-cars.py
+++ array/points-that-intersect-with-cars.py
@@ -5,20 +5,20 @@ class Solution:
             return nums[0][1] - nums[0][0] + 1
         
         nums.sort(key = lambda x: (x[0], x[1]))
-        cur_set = set(range(nums[0][0], nums[0][1] + 1))
+        # cur_set = set(range(nums[0][0], nums[0][1] + 1))
         counter = nums[0][1] + 1 - nums[0][0]
         # print(cur_set)
 
         # overlaps = set()
         for i in range(1, N):
             new_count = nums[i][1] + 1 - nums[i][0]
-            new_set = set(range(nums[i][0], nums[i][1] + 1))
+            # new_set = set(range(nums[i][0], nums[i][1] + 1))
             # print(new_set)
 
             overlaps = nums[i - 1][1] - nums[i][0] + 1 if nums[i - 1][1] >= nums[i][0] else 0
             # print(overlaps)
             counter += new_count - overlaps
-            cur_set |= new_set
-            print(cur_set)
+            # cur_set |= new_set
+            # print(cur_set)
 
         return counter
