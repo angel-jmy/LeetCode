209b875f18ab6cf390f9c218bdf99cfde336a9b1
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-11-01 16:15:25 -0700

    LC-points-that-intersect-with-cars [Runtime: 91 ms; Memory: 18.1 MB]

---
+++ hash-table/points-that-intersect-with-cars.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numberOfPoints(self, nums: List[List[int]]) -> int:
+        N = len(nums)
+        if N == 1:
+            return nums[0][1] - nums[0][0] + 1
+        
+        nums.sort(key = lambda x: (x[0], x[1]))
+        cur_set = set(range(nums[0][0], nums[0][1] + 1))
+        counter = nums[0][1] + 1 - nums[0][0]
+        print(cur_set)
+
+        # overlaps = set()
+        for i in range(1, N):
+            new_count = nums[i][1] + 1 - nums[i][0]
+            new_set = set(range(nums[i][0], nums[i][1] + 1))
+            print(new_set)
+            overlaps = cur_set & new_set
+            print(overlaps)
+            counter += new_count - len(overlaps)
+            cur_set |= new_set
+            print(cur_set)
+
+        return counter
