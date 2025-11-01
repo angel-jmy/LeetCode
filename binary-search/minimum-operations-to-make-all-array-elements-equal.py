97710bdaf5ba3f9a0dc72d71b3da83917995d294
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-31 18:44:51 -0700

    LC-minimum-operations-to-make-all-array-elements-equal [Runtime: N/A; Memory: N/A]

--- binary-search/minimum-operations-to-make-all-array-elements-equal.py
+++ binary-search/minimum-operations-to-make-all-array-elements-equal.py
@@ -2,7 +2,20 @@ class Solution:
     def minOperations(self, nums: List[int], queries: List[int]) -> List[int]:
         nums.sort()
         N = len(nums)
-    
+        prefs = [0] * N
+        prefs[0] = nums[0]
+        for i in range(1, N):
+            prefs[i] = prefs[i - 1] + nums[i]
+
+        suffs = [0] * N
+        suffs[N - 1] = nums[N - 1]
+        for i in range(N - 2, -1, -1):
+            suffs[i] = suffs[i + 1] + nums[i]
+
+        print(nums)
+        print(prefs)
+        print(suffs)
+        
         def binary_search(target):
             l, r = 0, N - 1
             while l <= r:
@@ -12,14 +25,18 @@ class Solution:
                 else:
                     l = mid + 1
             return r
-        
-        print(binary_search(8))
+
+        print(binary_search(1))
 
         res = []
         for q in queries:
+            # print(q)
             idx = binary_search(q)
             print(idx)
-            ops = prefs[idx] + suffs[idx]
+            if idx + 1 < N:
+                ops = q * (idx + 1) - prefs[idx] + suffs[idx + 1] - q * (N - idx - 1)
+            else:
+                ops = q * (idx + 1) - prefs[idx]
             res.append(ops)
 
         return res
