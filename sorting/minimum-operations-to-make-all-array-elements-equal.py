f51e2f46b257f547d39fe8f46254a818e461d7b2
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-10-31 19:17:17 -0700

    LC-minimum-operations-to-make-all-array-elements-equal [Runtime: 672 ms; Memory: 46.3 MB]

--- sorting/minimum-operations-to-make-all-array-elements-equal.py
+++ sorting/minimum-operations-to-make-all-array-elements-equal.py
@@ -15,19 +15,11 @@ class Solution:
             # print(q)
             i = bisect_right(nums, q)
             sum_left = prefs[i - 1] if i > 0 else 0
-            sum_right = total - (prefs[i - 1] if i > 0 else 0)
+            sum_right = total - sum_left
 
             left_ops = q * i - sum_left
             right_ops = sum_right - q * (N - i)
             res.append(left_ops + right_ops)
-
-            # if q < nums[0]:
-            #     ops = suffs[0] - q * N
-            # elif idx + 1 < N:
-            #     ops = q * (idx + 1) - prefs[idx] + suffs[idx + 1] - q * (N - idx - 1)
-            # else:
-            #     ops = q * (idx + 1) - prefs[idx]
-            # res.append(ops)
 
         return res
 
