9b6530f6098b4ee9e9733953f605f8ff9dc9c1da
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-11-02 20:58:14 -0800

    LC-sliding-subarray-beauty [Runtime: N/A; Memory: N/A]

---
+++ hash-table/sliding-subarray-beauty.py
@@ -0,0 +1,27 @@
+class Solution:
+    def getSubarrayBeauty(self, nums: List[int], k: int, x: int) -> List[int]:
+        N = len(nums)
+        res = [0] * (N - k + 1)
+
+        counts = Counter(nums[:k])
+        counter = 0
+
+        for num in range(-50, 0):
+            counter += counts[num]
+            if counter >= x:
+                res[0] = num
+                break
+
+        for i in range(1, N - k + 1):
+            counts[nums[i - 1]] -=1
+            counts[nums[i + k - 1]] += 1
+            counter = 0
+            print(counts)
+            for num in range(-50, 0):
+                counter += counts[num]
+                # print(counter, num)
+                if counter >= x:
+                    res[i] = num
+                    break
+
+        return res
