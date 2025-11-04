bb0b80d6ba20697bb71771fdf55b1e7e82d40560
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-11-03 23:34:03 -0800

    LC-take-k-of-each-character-from-left-and-right [Runtime: N/A; Memory: N/A]

---
+++ sliding-window/take-k-of-each-character-from-left-and-right.py
@@ -0,0 +1,24 @@
+class Solution:
+    def takeCharacters(self, s: str, k: int) -> int:
+        N = len(s)
+        tot_count = Counter(s)
+        if len(tot_count) < 3:
+            return -1
+
+        window = defaultdict(int)
+        for c in ['a', 'b', 'c']:
+            if tot_count[c] < k:
+                return -1
+
+        max_len = 0
+        l = 0
+        for r in range(N):
+            window[s[r]] += 1
+            while window['a'] > tot_count['a'] - k or window['b'] > tot_count['b'] - k or window['c'] > tot_count['c'] - k:
+                if l <= r:
+                    window[s[l]] -= 1
+                    l += 1
+
+            max_len = max(max_len, r - l + 1)
+
+        return N - max_len if max_len else -1
