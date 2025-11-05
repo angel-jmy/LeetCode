f64c5924905dd44cba0585fb72d1ea9708f3533a
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-11-04 21:55:14 -0800

    LC-find-the-longest-equal-subarray [Runtime: N/A; Memory: N/A]

---
+++ sliding-window/find-the-longest-equal-subarray.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestEqualSubarray(self, nums: List[int], k: int) -> int:
+        N = len(nums)
+        max_freq = 0
+        l = 0
+        window = defaultdict(int)
+
+        for r in range(N):
+            window[nums[r]] += 1
+            max_freq = max(max_freq, window[nums[r]])
+            while r - l + 1 - max_freq > k:
+                if nums[l] == nums[r]:
+                    max_freq -= 1
+                window[nums[l]] -= 1
+                l += 1
+            
+        return max_freq
