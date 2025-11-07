a0b91b094e6a8886b352016a6509b4844fb863c7
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-11-06 19:25:45 -0800

    LC-minimum-depth-of-binary-tree [Runtime: 39 ms; Memory: 50.4 MB]

--- depth-first-search/minimum-depth-of-binary-tree.py
+++ depth-first-search/minimum-depth-of-binary-tree.py
@@ -12,7 +12,7 @@ class Solution:
             return 1
         if not root.left:
             return 1 + self.minDepth(root.right)
-        if not root.left:
+        if not root.right:
             return 1 + self.minDepth(root.left)
 
         return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
