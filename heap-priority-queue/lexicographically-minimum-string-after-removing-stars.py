8f4d4dde8ec89aaff852b6d91372c6c2a8696891
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-11-06 22:17:56 -0800

    LC-lexicographically-minimum-string-after-removing-stars [Runtime: 897 ms; Memory: 23.7 MB]

--- heap-priority-queue/lexicographically-minimum-string-after-removing-stars.py
+++ heap-priority-queue/lexicographically-minimum-string-after-removing-stars.py
@@ -2,35 +2,29 @@ class Solution:
     def clearStars(self, s: str) -> str:
         N = len(s)
         pos = [[] for _ in range(26)]
-        counts = [0] * 26
-        tot_len = 0
+        # counts = [0] * 26
+        # tot_len = 0
+        deleted = set()
 
         for i in range(N):
             c = s[i]
             if c != '*':
                 index = ord(c) - ord('a')
                 pos[index].append(i)
-                counts[index] += 1
-                tot_len += 1
+
             else:
                 for j in range(26):
                     char = chr(j + ord('a'))
                     if pos[j]:
-                        pos[j].pop()
-                        tot_len -= 1
-                        counts[j] -= 1
+                        idx = pos[j].pop()
+                        deleted.add(idx)
                         break
 
-        print(tot_len)
-        print(pos)
-        res = ['0'] * tot_len
+        res = []
 
-        for i in range(26):
-            char = chr(i + ord('a'))
-            print(pos[i])
-            if pos[i]:
-                for idx in pos[i]:
-                    res[idx] = char
+        for i in range(N):
+            if s[i] != '*' and i not in deleted:
+                res.append(s[i])
 
         return "".join(res)
 
