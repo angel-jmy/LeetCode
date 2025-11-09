60f2c238c288c58e2887b5d648e5e448d021b0e8
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-11-09 13:04:02 -0800

    LC-replace-non-coprime-numbers-in-array [Runtime: 143 ms; Memory: 31.5 MB]

--- stack/replace-non-coprime-numbers-in-array.py
+++ stack/replace-non-coprime-numbers-in-array.py
@@ -1,13 +1,24 @@
 class Solution:
     def replaceNonCoprimes(self, nums: List[int]) -> List[int]:
         res = []
-        for num in nums:
+        N = len(nums)
+        i = 0
+        last_merge = False
+        while i < N:
+            if last_merge:
+                num = to_merge
+            else:
+                num = nums[i]
+
             if res and gcd(res[-1], num) != 1:
                 num0 = res.pop()
                 GCD = gcd(num0, num)
-                res.append(num0 * num // GCD)
+                to_merge = num0 * num // GCD
+                last_merge = True
             else:
                 res.append(num)
+                last_merge = False
+                i += 1
 
         return res
-        
+
