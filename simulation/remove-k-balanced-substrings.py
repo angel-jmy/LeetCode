81f84a6721240a3a8ee892c5ef4fd54de8a27605
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-11-09 11:58:52 -0800

    LC-remove-k-balanced-substrings [Runtime: N/A; Memory: N/A]

---
+++ simulation/remove-k-balanced-substrings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeSubstring(self, s: str, k: int) -> str:
+        stack = []
+        for c in s:
+            if stack and c == stack[-1][0]:
+                stack[-1][1] += 1
+                
+            else:
+                stack.append([c, 1])
+
+            if c == ')' and stack[-1][1] == k:
+                if stack[:-1] and stack[-2][0] == '(' and stack[-2][1] >= k:
+                    stack.pop()
+                    stack[-1][1] -= k
+
+        return ''.join(c * cnt for c, cnt in stack)
