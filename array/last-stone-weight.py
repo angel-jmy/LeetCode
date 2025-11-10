6d22c9551bdd08323c01b5639f30d67388ff98de
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-11-09 18:55:56 -0800

    LC-last-stone-weight [Runtime: 4 ms; Memory: 17.8 MB]

---
+++ array/last-stone-weight.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        tot_len = len(stones)
+        hq = [-s for s in stones]
+        heapq.heapify(hq)
+
+        while tot_len >= 2:
+            y = -heapq.heappop(hq)
+            x = -heapq.heappop(hq)
+            print(x, y)
+            if x == y:
+                tot_len -= 2
+            else:
+                heapq.heappush(hq, x - y)
+                tot_len -= 1
+
+        return -hq[0] if hq else 0
