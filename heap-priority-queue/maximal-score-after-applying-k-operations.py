f971e92f2c9ea64705da1a86cffdd916e6b984a7
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-11-10 21:01:54 -0800

    LC-maximal-score-after-applying-k-operations [Runtime: 267 ms; Memory: 33.3 MB]

--- heap-priority-queue/maximal-score-after-applying-k-operations.py
+++ heap-priority-queue/maximal-score-after-applying-k-operations.py
@@ -1,12 +1,11 @@
 class Solution:
     def maxKelements(self, nums: List[int], k: int) -> int:
-        N = len(nums)
+        # N = len(nums)
         nums = [-num for num in nums]
         heapq.heapify(nums)
 
         score = 0
-        k = min(k, N)
-
+        
         for _ in range(k):
             num = -heapq.heappop(nums)
             score += num
