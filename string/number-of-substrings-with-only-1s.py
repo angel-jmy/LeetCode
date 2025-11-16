b7f07085c2f349f9ba57ba2bf5357f95d4731cef
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-11-16 13:31:35 -0800

    LC-number-of-substrings-with-only-1s [Runtime: 23 ms; Memory: 18.1 MB]

--- string/number-of-substrings-with-only-1s.py
+++ string/number-of-substrings-with-only-1s.py
@@ -1,5 +1,6 @@
 class Solution:
     def numSub(self, s: str) -> int:
+        MOD = 10 ** 9 + 7
         N = len(s)
         l = 0
         # cur_len = 0
@@ -8,12 +9,12 @@ class Solution:
             if s[r] == '0':
                 if r > 0 and s[r - 1] == '1':
                     n = r - l
-                    counter += n * (n + 1) // 2
+                    counter = (counter + n * (n + 1) // 2) % MOD
                 l = r + 1
         
         # print(l, r)
         if s[r] == '1':
             n = r - l + 1
-            counter += n * (n + 1) // 2
+            counter = (counter + n * (n + 1) // 2) % MOD
 
         return counter
