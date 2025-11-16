87d8f082ca3c0155c1e1dceb9440173e37d585b2
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-11-15 23:26:15 -0800

    LC-longest-subsequence-with-limited-sum [Runtime: 4 ms; Memory: 17.9 MB]

--- prefix-sum/longest-subsequence-with-limited-sum.py
+++ prefix-sum/longest-subsequence-with-limited-sum.py
@@ -1,26 +1,16 @@
 class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
-        N = len(nums)
+        M = len(queries)
         nums.sort()
-        pref = [0] * (N + 1)
-
-        for i in range(N):
-            pref[i + 1] = pref[i] + nums[i]
-
-        def binarySearch(target):
-            l, r = 0, N
-            while l <= r:
-                mid = l + (r - l) // 2
-                if pref[mid] > target:
-                    r = mid - 1
-                else:
-                    l = mid + 1
-
-            return r
-
-        res = []
+        N = len(nums)
+        prefs = [0] * N 
+        prefs[0] = nums[0]
+        for i in range(1, N):
+            prefs[i] = prefs[i - 1] + nums[i]
+        
+        answer = []
         for q in queries:
-            res.append(binarySearch(q))
+            idx = bisect_right(prefs, q)
+            answer.append(idx)
 
-        return res
-                
+        return answer
