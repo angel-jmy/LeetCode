41a8b14f4d9bf45841629f39a648ef1b79e485a1
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-11-15 22:14:07 -0800

    LC-employees-earning-more-than-their-managers [Runtime: 308 ms; Memory: 68.8 MB]

--- database/employees-earning-more-than-their-managers.py
+++ database/employees-earning-more-than-their-managers.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
-    joined = employee.merge(employee[['id', 'name', 'salary']], \
+    joined = employee.merge(employee[['id', 'salary']], \
                     left_on = 'managerId', right_on = 'id',
                     suffixes = ('_e', '_m'))
     
-    return joined[joined['salary_e'] > joined['salary_m']][['name_e']].rename(columns = {'name_e':'Employee'})
+    return joined[joined['salary_e'] > joined['salary_m']][['name']].rename(columns = {'name':'Employee'})
