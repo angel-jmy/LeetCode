276ee9abc11a1aff4f3adc17236260b36f437356
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-11-16 22:49:18 -0800

    LC-element-appearing-more-than-25-in-sorted-array [Runtime: 0 ms; Memory: 18.8 MB]

--- array/element-appearing-more-than-25-in-sorted-array.py
+++ array/element-appearing-more-than-25-in-sorted-array.py
@@ -1,7 +1,7 @@
 class Solution:
     def findSpecialInteger(self, arr: List[int]) -> int:
         N = len(arr)
-        candidates = [arr[N // 4], arr[N // 2], arr[N // 4 * 3]]
+        candidates = [arr[N // 4], arr[N // 2], arr[3 * N // 4]]
         print(candidates)
         for c in candidates:
             lb = bisect_left(arr, c)
