651a2ce0bcc732878da3ded652d1f791e85e2f27
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-11-16 22:29:05 -0800

    LC-online-election [Runtime: 103 ms; Memory: 23.7 MB]

--- array/online-election.py
+++ array/online-election.py
@@ -2,26 +2,26 @@ class TopVotedCandidate:
 
     def __init__(self, persons: List[int], times: List[int]):
         N = len(persons)
-        self.prefs = [0] * N
-        if persons[0] == 1:
-            self.prefs[0] = 1
-        else:
-            self.prefs[0] = -1
-        for i in range(1, N):
-            num = 1 if persons[i] == 1 else -1
-            self.prefs[i] = self.prefs[i - 1] + num
+        self.prefs = [0] * N # id of the max_count candidate so far
+        self.prefs[0] = persons[0]
+        # max_person = 0
+        max_count = 0
+        counts = defaultdict(int)
+        for i in range(N):
+            counts[persons[i]] += 1
+            if counts[persons[i]] >= max_count:
+                max_count = counts[persons[i]]
+                self.prefs[i] = persons[i]
+            elif i > 0:
+                self.prefs[i] = self.prefs[i - 1]
 
-        self.persons = persons
+        # self.persons = persons
         self.times = times
 
     def q(self, t: int) -> int:
         idx = bisect_right(self.times, t) - 1
-        if self.prefs[idx] > 0:
-            return 1
-        if self.prefs[idx] < 0:
-            return 0
         
-        return self.persons[idx]
+        return self.prefs[idx]
 
         
 
