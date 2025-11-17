76c8c40afddf4e6b133bc43ef70c70f04614545d
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-11-16 21:47:39 -0800

    LC-minimum-absolute-sum-difference [Runtime: 415 ms; Memory: 41.3 MB]

--- sorting/minimum-absolute-sum-difference.py
+++ sorting/minimum-absolute-sum-difference.py
@@ -1,30 +1,32 @@
 class Solution:
     def minAbsoluteSumDiff(self, nums1: List[int], nums2: List[int]) -> int:
         MOD = 10 ** 9 + 7
-        N = len(nums1)
-        ab_sum = 0
-        max_diff = 0
-        max_idx = 0
-        for i in range(N):
-            diff = abs(nums1[i] - nums2[i])
-            if diff > max_diff:
-                max_diff = diff
-                max_idx = i
-            ab_sum = (ab_sum + diff) % MOD
-
-        if ab_sum == 0:
+        if nums1 == nums2:
             return 0
-
-        target = nums2[max_idx]
-        nums1.sort()
-        idx = bisect_left(nums1, target)
-        if idx >= N:
-            idx = N - 1
-        elif idx > 0:
-            if target - nums1[idx - 1] < nums1[idx] - target:
-                idx = idx - 1
+        N = len(nums1)
+        max_red = 0
+        combined = [(n1, n2) for n1, n2 in zip(nums1, nums2)]
+        combined.sort(key = lambda x: x[0])
+        nums1 = [n1 for n1, n2 in combined]
+        nums2 = [n2 for n1, n2 in combined]
+        def find_closest(t):            
+            idx = bisect_left(nums1, t)
+            if idx >= N:
+                idx = N - 1
+            elif idx > 0:
+                if t - nums1[idx - 1] < nums1[idx] - t:
+                    idx = idx - 1
+            return idx
         
-        ab_sum -= max_diff
-        ab_sum = (ab_sum + abs(target - nums1[idx])) % MOD
+        for i in range(N):
+            idx = find_closest(nums2[i])
+            if i == idx:
+                continue
+            red = abs(nums2[i] - nums1[i]) - abs(nums2[i] - nums1[idx])
+            max_red = max(max_red, red)
+
+        print(max_red)
+        print(sum([abs(nums1[i] - nums2[i]) for i in range(N)]))
+        ab_sum = (sum([abs(nums1[i] - nums2[i]) for i in range(N)]) - max_red) % MOD
 
         return ab_sum
