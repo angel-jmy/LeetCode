7c03bc9c8fa45d52e6b48ee13598179eaecbdccb
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-11-18 01:15:02 -0800

    LC-binary-tree-right-side-view [Runtime: 0 ms; Memory: 17.9 MB]

--- breadth-first-search/binary-tree-right-side-view.py
+++ breadth-first-search/binary-tree-right-side-view.py
@@ -8,18 +8,21 @@ class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
             return []
-
+        
         res = []
         queue = deque([root])
-
+        
         while queue:
-            for i in range(len(queue)):
+            level_size = len(queue)
+            for i in range(level_size):
                 node = queue.popleft()
-                if i == 0:
+                # Capture the last node in this level
+                if i == level_size - 1:
                     res.append(node.val)
-                if node.right:
-                    queue.append(node.right)
+                # Add children in standard order (left then right)
                 if node.left:
                     queue.append(node.left)
-            
+                if node.right:
+                    queue.append(node.right)
+        
         return res
