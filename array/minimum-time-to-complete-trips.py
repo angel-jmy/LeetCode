0860c80ea9da7e821cd981ebed94f0b0d17a5dab
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-11-22 00:37:19 -0800

    LC-minimum-time-to-complete-trips [Runtime: N/A; Memory: N/A]

---
+++ array/minimum-time-to-complete-trips.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minimumTime(self, time: List[int], totalTrips: int) -> int:
+        
+        def get_trips(t):
+            trips = [t // ti for ti in time]
+            return sum(trips)
+
+        l = 1
+        r = totalTrips // min(time) + 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            tot_trips = get_trips(mid)
+            if tot_trips < totalTrips:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return l 
+        
