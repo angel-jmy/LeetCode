9e0d05c4c98f1196d32aca5d81cf713f2808c847
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-11-22 00:39:15 -0800

    LC-minimum-time-to-complete-trips [Runtime: 993 ms; Memory: 31.2 MB]

--- binary-search/minimum-time-to-complete-trips.py
+++ binary-search/minimum-time-to-complete-trips.py
@@ -6,7 +6,7 @@ class Solution:
             return sum(trips)
 
         l = 1
-        r = totalTrips // min(time) + 1
+        r = totalTrips * max(time)
         while l <= r:
             mid = l + (r - l) // 2
             tot_trips = get_trips(mid)
@@ -14,5 +14,6 @@ class Solution:
                 l = mid + 1
             else:
                 r = mid - 1
+        
         return l 
         
