9c4199926b007e6e3ddda6a7db5c67f829f7bc5f
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-11-22 21:03:36 -0800

    LC-remove-linked-list-elements [Runtime: 3 ms; Memory: 19.6 MB]

---
+++ recursion/remove-linked-list-elements.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        curr = head
+        head = ListNode(0)
+        head.next = curr
+        pointer = head
+        while pointer.next:
+            if pointer.next.val == val:
+                pointer.next = pointer.next.next
+            else:
+                pointer = pointer.next
+
+        return head.next
