518c36e730c79df72f473976619d2a6338b316f6
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-12-02 16:22:24 -0800

    LC-isomorphic-strings [Runtime: 3 ms; Memory: 18.2 MB]

--- hash-table/isomorphic-strings.py
+++ hash-table/isomorphic-strings.py
@@ -2,10 +2,15 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         N = len(s)
         mapping = {}
+        mapped = set()
         for i in range(N):
             c1, c2 = s[i], t[i]
             if c1 not in mapping:
+                if c2 in mapped:
+                    return False
+                    
                 mapping[c1] = c2
+                mapped.add(c2)
             else:
                 if mapping[c1] != c2:
                     return False
