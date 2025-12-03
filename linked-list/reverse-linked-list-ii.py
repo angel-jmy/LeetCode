b1baf472e4d0474e8fcb30c58c3f003be9c5f98c
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-12-03 14:16:38 -0800

    LC-reverse-linked-list-ii [Runtime: N/A; Memory: N/A]

---
+++ linked-list/reverse-linked-list-ii.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        idx = 1
+        dummy = ListNode(0, head)
+
+        curr = dummy
+    
+        while curr.next:
+            if idx < left:
+                curr = curr.next
+                idx += 1
+            elif idx == left:
+                prev = curr
+                mid = ListNode(curr.next.val)
+                curr = curr.next
+                idx += 1
+            elif left < idx <= right:
+                mid = ListNode(curr.next.val, mid)
+                curr = curr.next
+                idx += 1
+            else:
+                prev.next = mid
+                while mid.next:
+                    mid = mid.next
+                mid.next = curr.next
+                break
+
+        return dummy.next
