d642aabe109bef240e80766c7b6156c871204d26
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-12-03 22:09:26 -0800

    LC-rotate-list [Runtime: N/A; Memory: N/A]

---
+++ linked-list/rotate-list.py
@@ -0,0 +1,42 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        curr = head
+        length = 1
+        while curr.next:
+            length += 1
+            curr = curr.next
+        
+        k = k % length
+        target = length - k
+        # print(target)
+        i = 0
+        dummy = ListNode(0, head)
+        slow = fast = dummy
+        while fast.next:
+            if i < target:
+                i += 1
+                fast = fast.next
+            elif i == target:
+                prev = fast
+                fast = fast.next
+                prev.next = None
+                break
+
+        new_head = fast
+        while fast.next:
+            fast = fast.next
+
+        fast.next = slow.next
+
+        return new_head
+
+
+
