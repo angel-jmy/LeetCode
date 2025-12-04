845d99369836ea3a0cd0b0b8c77a9dfb11635f8e
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-12-04 10:57:57 -0800

    LC-swapping-nodes-in-a-linked-list [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/swapping-nodes-in-a-linked-list.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        length = 0
+        dummy = ListNode(0, head)
+        curr = dummy
+        while curr.next:
+            length += 1
+            if length == k:
+                val1 = curr.next.val
+                slow = curr
+            curr = curr.next
+
+        tgt = length - k
+        curr = dummy
+        i = 0
+        while curr.next:
+            if i < tgt:
+                i += 1
+                curr = curr.next
+            elif i == tgt:
+                fast = curr
+                val2 = curr.next.val
+                fast.next = ListNode(val1, fast.next.next)
+                break
+
+        slow.next = ListNode(val2, slow.next.next)
+
+        return dummy.next
