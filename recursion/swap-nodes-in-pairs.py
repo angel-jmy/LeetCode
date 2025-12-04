8de224629890e327b94080479fce727c67d4decc
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-12-03 20:42:44 -0800

    LC-swap-nodes-in-pairs [Runtime: 0 ms; Memory: 17.9 MB]

--- recursion/swap-nodes-in-pairs.py
+++ recursion/swap-nodes-in-pairs.py
@@ -1,35 +1,19 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def swapPairs(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        if not head or not head.next:
-            return head
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        curr = dummy
 
-        curr = head          # keep pointer to original head
-        head = head.next     # new head is the second node
-        prev = None          # tail of the last swapped pair
+        while curr.next and curr.next.next:
+            after = curr.next.next.next
+            val1 = curr.next.val
+            val2 = curr.next.next.val
+            curr.next = ListNode(val2)
+            curr.next.next = ListNode(val1, after)
+            curr = curr.next.next
 
-        while curr and curr.next:
-            temp = curr.next     # second in the pair
-            tail = temp.next     # head of the remaining list
-
-            # swap the pair (temp <-> curr)
-            temp.next = curr
-            curr.next = tail
-
-            # connect previous pair to current swapped pair
-            if prev:
-                prev.next = temp
-
-            # advance pointers
-            prev = curr
-            curr = tail
-
-        return head
+        return dummy.next
