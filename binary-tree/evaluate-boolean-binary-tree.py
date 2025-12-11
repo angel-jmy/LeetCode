7e89f4647738ce0dbea80a1179e74480c05691f9
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-12-10 23:33:53 -0800

    LC-evaluate-boolean-binary-tree [Runtime: N/A; Memory: N/A]

---
+++ binary-tree/evaluate-boolean-binary-tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        # boolean = True
+        def inorder(node):
+            # nonlocal boolean
+            if not node:
+                return True
+            if node.val == 0:
+                return False
+            if node.val == 1:
+                return True
+
+            if node.val == 2:
+                return inorder(node.left) or inorder(node.right)
+            else:
+                return inorder(node.left) or inorder(node.right)
+
+        return inorder(root)
