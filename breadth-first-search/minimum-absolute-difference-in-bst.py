a362da8939cd063cb77dce90f47afdda7d224667
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-12-13 18:58:10 -0800

    LC-minimum-absolute-difference-in-bst [Runtime: N/A; Memory: N/A]

---
+++ breadth-first-search/minimum-absolute-difference-in-bst.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        
+        nodes = []
+        def preorder(node):
+            if not node:
+                return
+
+            heapq.heappush(nodes, node.val)
+            preorder(node.left)
+            preorder(node.right)
+
+        preorder(root)
+
+        node0 = heapq.heappop(nodes)
+        diff = 10 ** 5
+        while nodes:
+            node1 = heapq.heappop(nodes)
+            diff = min(diff, node1 - node0)
+
+        return diff
+
+
+
