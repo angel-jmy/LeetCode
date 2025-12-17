d1b93e9bc718edc9ec0b5fdd2617447706d6c396
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2025-12-16 21:29:51 -0800

    LC-longest-zigzag-path-in-a-binary-tree [Runtime: N/A; Memory: N/A]

--- tree/longest-zigzag-path-in-a-binary-tree.py
+++ tree/longest-zigzag-path-in-a-binary-tree.py
@@ -8,29 +8,53 @@ class Solution:
     def longestZigZag(self, root: Optional[TreeNode]) -> int:
         length = 0
 
-        def dfs(node, left, d):
+        def dfs1(node, left, d):
             nonlocal length
             if not node:
                 return
             if left: 
                 if node.right:
                     length = max(length, d + 1)
-                    dfs(node.right, False, d + 1)
+                    dfs1(node.right, False, d + 1)
                 else:
-                    dfs(node.left, True, 0)
-                    dfs(node.left, False, 0)
+                    dfs1(node.left, True, 0)
+                    dfs1(node.left, False, 0)
 
             else:
                 if node.left:
                     length = max(length, d + 1)
-                    dfs(node.left, True, d + 1)
+                    dfs1(node.left, True, d + 1)
                 else:
-                    dfs(node.right, True, 0)
-                    dfs(node.right, False, 0)
+                    dfs1(node.right, True, 0)
+                    dfs1(node.right, False, 0)
 
 
-        dfs(root, True, 0)
-        dfs(root, False, 0)
+        def dfs2(node, left, d):
+            nonlocal length
+            if not node:
+                return
+            if left: 
+                if node.left:
+                    length = max(length, d + 1)
+                    dfs2(node.left, False, d + 1)
+                else:
+                    dfs2(node.right, True, 0)
+                    dfs2(node.right, False, 0)
+
+            else:
+                if node.right:
+                    length = max(length, d + 1)
+                    dfs2(node.right, True, d + 1)
+                else:
+                    dfs2(node.left, True, 0)
+                    dfs2(node.left, False, 0)
+
+
+        dfs1(root, True, 0)
+        dfs1(root, False, 0)
+
+        dfs2(root, True, 0)
+        dfs2(root, False, 0)
 
         return length
 
