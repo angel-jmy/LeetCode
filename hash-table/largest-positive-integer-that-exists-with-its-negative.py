f14b0ed971588eb64cb2e7320ea46d9c35a18345
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2026-01-18 21:40:47 -0500

    LC-largest-positive-integer-that-exists-with-its-negative [Runtime: 6 ms; Memory: 19.4 MB]

--- hash-table/largest-positive-integer-that-exists-with-its-negative.py
+++ hash-table/largest-positive-integer-that-exists-with-its-negative.py
@@ -1,13 +1,11 @@
 class Solution:
     def findMaxK(self, nums: List[int]) -> int:
-        seen = set()
-        largest = -1
+        res = -1
+        all_num = set()
         for num in nums:
-            if num not in seen and -num not in seen:
-                seen.add(num)
-            elif num not in seen and -num in seen:
-                seen.add(num)
-                largest = max(largest, abs(num))
+            if -num in all_num:
+                res = max(res, abs(num))
 
+            all_num.add(num)
 
-        return largest
+        return res
