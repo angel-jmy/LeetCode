4c84e4a9f9bf0056a9c5876ba268e0d741f902a0
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2026-01-18 23:18:55 -0500

    LC-human-traffic-of-stadium [Runtime: 382 ms; Memory: 0B]

--- database/human-traffic-of-stadium.sql
+++ database/human-traffic-of-stadium.sql
@@ -1,25 +1,26 @@
--- Write your PostgreSQL query statement below
-with flagged as (
+# Write your MySQL query statement below
+with flags as (
     select id, visit_date, people, 
-     case when people < 100 then 1 else 0 end as flag
+    case when people < 100 then 1 else 0 end as flag
     from Stadium
 ),
 
-separated as (
-    select id, visit_date, people,
-     sum(flag) over (order by visit_date asc rows between unbounded preceding and current row) as grp
-    from flagged
+grps as (
+    select id, visit_date, people, 
+    sum(flag) over (order by visit_date asc) as grp
+    from flags
 ),
 
 grouped as (
-    select grp, count(*) as cnt
-    from separated
+    select grp, count(*) as days
+    from grps
     where people >= 100
     group by grp
+    having count(*) >= 3
 )
 
-select s.id, s.visit_date, s.people
-from separated s
-join grouped g
-on s.grp = g.grp
-where g.cnt >= 3 and s.people >= 100;
+select id, visit_date, people
+from grps
+join grouped
+on grps.grp = grouped.grp
+where people >= 100
