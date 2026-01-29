385c3230c896a46c4b9492f12d0c59d75f1cbef9
Mengying Ju <38664041+angel-jmy@users.noreply.github.com>
2026-01-29 13:14:22 -0500

    LC-continuous-subarray-sum [Runtime: 184 ms; Memory: 51.1 MB]

--- prefix-sum/continuous-subarray-sum.py
+++ prefix-sum/continuous-subarray-sum.py
@@ -7,7 +7,7 @@ class Solution:
             prefs[i] = prefs[i - 1] + nums[i - 1]
 
         mods = defaultdict(list)
-        for i in range(1, N + 1):
+        for i in range(N + 1):
             new_mod = prefs[i] % k
             if new_mod in mods:
                 length = len(mods[new_mod])
